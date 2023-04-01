71f7de25b9024e6910863563826ffe5400854353
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2023-04-01 08:24:28 -0500

    styling figured out

---
+++ components/Card.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../styles/Card.module.css'
+
+export default function Card() {
+  return (
+    <>
+    <div className={styles.container}>
+    <h1 className={styles.title}>You are on the right page.</h1>
+    </div>
+     
+    </>
+   
+  )
+}
+
+
---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <h1>This is the about page</h1>
+  )
+}
