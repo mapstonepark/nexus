866f756d5edf1d6b12abb0f3a2d1002a8b897c70
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2023-04-10 21:19:28 -0500

    playing with forms for inputting to database

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
 import Card from '../components/Card'
+import Form from '../components/Form'
+
 
 export default function about() {
   return (
-    <Card />
+   <Form />
   )
 }
